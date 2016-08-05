a878990d59e4444925ab251f37ebf0c6e5068f46
Ezra Raez <ezRAez10@gmail.com>
2016-08-05 15:28:54 -0700

    update production rb and seeds" ;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
     :path => '/:class/:attachment/:id_partition/:style/:filename',
   }
 
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.assets.compile = true
 
   # Code is not reloaded between requests.
